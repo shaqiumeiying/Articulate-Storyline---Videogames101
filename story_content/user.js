db6e0c13a5619b189e12867d44f7dc6fab362f96
shaqiumeiying <shaqiumeiying@gmail.com>
2022-09-13 19:36:04 -0700

    articulate

---
+++ story_content/user.js
@@ -0,0 +1,21 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5eoj5oZArKG":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  var currentTime = new Date()
+var month = currentTime.getMonth() + 1
+var day = currentTime.getDate()
+var year = currentTime.getFullYear()
+var dateString=month + "/" + day + "/" + year
+var player = GetPlayer();
+player.SetVar("SystemDate",dateString);
+}
+
